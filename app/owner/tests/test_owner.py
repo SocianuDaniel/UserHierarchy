99d0588331653ec770a869fcbad307434b6ebd45
Daniel Socianu <socianudaniel@gmail.com>
2024-10-12 09:00:15 +0200

    owner register complete

--- app/owner/tests/test_owner.py
+++ app/owner/tests/test_owner.py
@@ -1,26 +1,31 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
-from core.models import Owner
-from core import forms
+# from django.contrib.auth import get_user_model
+# from core.models import Owner
+# from core import forms
+
 
 class OwnerTest(TestCase):
-    """Test for owner"""
+    """ Test for owner """
 
     def test_error_if_user_exists(self):
-        """Test if throw error if user exists"""
+        """ Test if throw error if user exists """
         payload = {
             'email': "test123@email.com",
             'password1': 'pass123',
             'password2': 'pass123'
         }
-        user = get_user_model().objects.create_user(
+        """ user = get_user_model().objects.create_user(
             email=payload['email'],
             password=payload['password1']
-            )
-        response = self.client.post('owner/register/',payload)
-        #form = forms.CreateOwnerForm(data=payload)
-        
-        self.assertFormError(response,'form','emai','user allready in the database')
-    
+            ) """
+        response = self.client.post('owner/register/', payload)
+        # form = forms.CreateOwnerForm(data=payload)
+
+        self.assertFormError(
+            response,
+            'form',
+            'emai',
+            'user allready in the database')
+
     def test_un_test(self):
-        self.assertEqual(1,2,"unu nu e egal cu doi")
+        self.assertEqual(1, 2, "unu nu e egal cu doi")
--- app/owner/urls.py
+++ app/owner/urls.py
@@ -1,10 +1,12 @@
 """ urls for the owner """
 from django.urls import path
-from .  import views
+from . import views
 from django.views.generic.base import TemplateView
 app_name = "owner"
 urlpatterns = [
     path('register/', views.register_owner, name='create'),
-    path('thanks/',TemplateView.as_view(template_name="owner/registration_complete.html"), name="registration-complete")
+    path('thanks/',
+         TemplateView.as_view(
+             template_name="owner/registration_complete.html"),
+         name="registration-complete")
 ]
-
